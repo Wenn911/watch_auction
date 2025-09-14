da65f6316d0dd4ded2c211eabd88f738b56df825
Wenn911 <Wenn123@yandex.ru>
2025-09-14 11:50:12 +0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+
+import { init, miniApp } from '@telegram-apps/sdk';
+
+
+const initializeTelegramSDK = async () => {
+  try {
+    await init();
+
+
+    if (miniApp.ready.isAvailable()) {
+      await miniApp.ready();
+      console.log('Mini App готово');
+    }
+
+
+  } catch (error) {
+    console.error('Ошибка инициализации:', error);
+  }
+};
+
+
+initializeTelegramSDK();
+
+const root = document.getElementById('root') as Element;
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
