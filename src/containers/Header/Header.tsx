dc450a0ce4a87fbe8ad28d9fc2a551ed0618aa6b
Wenn911 <Wenn123@yandex.ru>
2025-09-18 18:37:41 +0300

    add back button

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,8 +1,21 @@
+import { useMemo } from "react";
+import { useLocation, useNavigate } from "react-router";
+
 import { chakra } from "@chakra-ui/react"
 
+import BackButtonIcon from "$/assets/backButton.svg?react"; 
 import LogoIcon from "$/assets/logo.svg?react"; 
 
 export const Header = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  
+  const showBackButton = useMemo(() => 
+    (!/\/(active)$/gi.test(pathname)),
+  [pathname]);
+
+  const goBack = () => navigate(-1);
+
   return (
     <chakra.div
       display="flex"
@@ -12,13 +25,23 @@ export const Header = () => {
       borderBottom="1px solid rgba(255, 255, 255, 0.2)"
     >
       <LogoIcon width="32" height="32" />
-      <chakra.text
-        fontSize="16px"
-        lineHeight="24px" 
-        fontWeight="500"
+      <chakra.button
+        visibility={showBackButton ? 'visible' : 'hidden'}
+        display="flex"
+        gap="8px"
+        textDecoration="none"
+        onClick={goBack}
+        alignItems="center"
       >
-        ChronoPenis
-      </chakra.text>
+        <BackButtonIcon />
+        <chakra.text 
+          fontSize="16px"
+          lineHeight="24px"
+          fontWeight="500"
+        >
+          Назад
+        </chakra.text>
+      </chakra.button>
     </chakra.div>
   )
 }
