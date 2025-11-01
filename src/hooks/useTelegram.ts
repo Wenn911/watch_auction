2943c3bb5085283d179eb13cffff9abb361d90a4
Wenn911 <Wenn123@yandex.ru>
2025-11-01 13:29:30 +0300

    тест пр

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -1,51 +1,51 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
 
 import type { WebApp, WebAppUser } from 'telegram-web-app';
 
 export const useTelegram = () => {
-  const [user, setUser] = useState<WebAppUser>();
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
-  const [isTelegram, setTelegram] = useState<boolean>(false);
-  const [webApp, setWebApp] = useState<WebApp>();
+    const [user, setUser] = useState<WebAppUser>();
+    const [theme, setTheme] = useState<'light' | 'dark'>('light');
+    const [isTelegram, setTelegram] = useState<boolean>(false);
+    const [webApp, setWebApp] = useState<WebApp>();
 
-  useEffect(() => {
-    const tg = window.Telegram?.WebApp;
-    
-    if (tg) {
-      setTelegram(true)
-      setWebApp(tg)
-      tg.ready();
-      setUser(tg.initDataUnsafe?.user);
-      setTheme(tg.colorScheme);
+    useEffect(() => {
+        const tg = window.Telegram?.WebApp;
 
-      const handleThemeChange = () => {
-        setTheme(tg.colorScheme);
-      };
+        if (tg) {
+            setTelegram(true);
+            setWebApp(tg);
+            tg.ready();
+            setUser(tg.initDataUnsafe?.user);
+            setTheme(tg.colorScheme);
 
-      tg.onEvent('themeChanged', handleThemeChange);
+            const handleThemeChange = () => {
+                setTheme(tg.colorScheme);
+            };
 
-      return () => {
-        tg.offEvent('themeChanged', handleThemeChange);
-      };
-    }
-  }, []);
+            tg.onEvent('themeChanged', handleThemeChange);
 
-  const showAlert = (message: string) => {
-    window.Telegram?.WebApp.showAlert(message);
-  };
+            return () => {
+                tg.offEvent('themeChanged', handleThemeChange);
+            };
+        }
+    }, []);
 
-  const closeApp = () => {
-    window.Telegram?.WebApp.close();
-  };
+    const showAlert = (message: string) => {
+        window.Telegram?.WebApp.showAlert(message);
+    };
 
-  return {
-    user,
-    theme,
-    showAlert,
-    closeApp,
-    isTelegram,
-    webApp
-  };
+    const closeApp = () => {
+        window.Telegram?.WebApp.close();
+    };
+
+    return {
+        user,
+        theme,
+        showAlert,
+        closeApp,
+        isTelegram,
+        webApp
+    };
 };
